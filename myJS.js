ca5f3ae933a397fa53036a30925b0858583d2092
Cayoo7777 <112037920+Cayoo7777@users.noreply.github.com>
2022-08-25 14:14:48 -0300

    Add files via upload

---
+++ myJS.js
@@ -0,0 +1,17 @@
+
+    function funcao1(){
+        alert("calma vida ainda não ta pronto");
+
+}
+
+    function redirecionar() {
+        //redireciona sem volta
+        window.location.replace("https://www.google.com.br");
+
+    }
+
+    function redirecionar2() {
+        //redireciona podendo voltar
+        window.location.href = "indexbody.html" ;
+
+    }
